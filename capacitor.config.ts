bfd9788b29f6c43398437c37eeffb40c342ef2e0
vale-nicole-cedi <13047@portalcedi.edu.mx>
2025-02-20 08:56:38 -0600

    1stcommit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EA11_IdeasAI',
+  webDir: 'www'
+};
+
+export default config;
